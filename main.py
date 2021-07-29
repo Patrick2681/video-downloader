3ebfb86d17a96cb73db615c87e0ee0a99eb49a24
Patrick Ferreira <patrickfg.ca@gmail.com>
2021-07-29 11:47:02 -0300

    v1 interface

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from ui_mainwidow import Ui_MainWindow
+from ui_mainwidow import Ui_Form
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_Form()
         self.ui.setupUi(self)
 
 if __name__ == '__main__':
